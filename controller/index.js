94cfbbbd13fa2adc7159f395f481e1ede1460fe8
anand <vermaanand278@gmail.com>
2024-11-19 17:28:30 +0530

    fsadf

--- controller/index.js
+++ controller/index.js
@@ -9,11 +9,6 @@ const { default: axios } = require("axios");
 let recurstionCount = 0;
 exports.generatedTimeEveryAfterEveryOneMin = (io) => {
   const job = schedule.schedule("* * * * * *", function () {
-    // const currentTime = new Date();
-    // const timeToSend =
-    //   currentTime.getSeconds() > 0
-    //     ? 60 - currentTime.getSeconds()
-    //     : currentTime.getSeconds();
     const now = new Date();
     const nowIST = soment(now).tz("Asia/Kolkata");
 
